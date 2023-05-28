3ed068bbd2c27b592d6c682e18c03d599d036a56
Tim Grimes <tim.grimes.3@gmail.com>
2023-05-27 22:35:06 -0500

    fix

--- keyboards/planck/keymaps/jozuman/keymap.c
+++ keyboards/planck/keymaps/jozuman/keymap.c
@@ -155,7 +155,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * CtlAlD = Ctrl + Alt + D + Custom Macro + More
  * MsCtrl = Mission Control
  */
-[_FUNCTION] = LAYOUT_planck_grid(
+[_FUNCTION_MAC] = LAYOUT_planck_grid(
     KC_1,     KC_MCTL,          KC_TRNS, KC_TRNS, KC_TRNS,      KC_5,  KC_6,  KC_7,          KC_7, KC_8,   KC_9,    KC_TRNS,
     KC_NO,    LCTL(LSFT(KC_1)), _______, _______, _______,      KC_T,  KC_Y,  KC_U,          KC_4, KC_5,   KC_6,    KC_PAST,
     KC_NO,    _______,          KC_S,    KC_D,    LSFT(KC_INS), KC_G,  KC_H,  KC_J,          KC_1, KC_2,   KC_3,    KC_PMNS,
